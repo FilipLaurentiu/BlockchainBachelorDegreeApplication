3074ebb8a7f755aeaaaf46e11616d2b0e28b96e4
FilipLaurentiu <f.laurentiu26@gmail.com>
2019-08-16 13:32:40 +0300

    Add consesnsus endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -166,7 +166,54 @@ app.post('/register-nodes-bulk', (req, res) => {
         }
     });
     res.json({ note: 'Bulk registration successful.' });
-})
+});
+
+
+app.get('/consensus', function (req, res) {
+    const requestPromises = [];
+    UCVcoin.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: networkNodeUrl + '/blockchain',
+            method: 'GET',
+            json: true
+        };
+
+        requestPromises.push(rp(requestOptions));
+    });
+
+    Promise.all(requestPromises)
+        .then(blockchains => {
+            const currentChainLength = UCVcoin.chain.length;
+            let maxChainLength = currentChainLength;
+            let newLongestChain = null;
+            let newPendingTransactions = null;
+
+            blockchains.forEach(blockchain => {
+                if (blockchain.chain.length > maxChainLength) {
+                    maxChainLength = blockchain.chain.length;
+                    newLongestChain = blockchain.chain;
+                    newPendingTransactions = blockchain.pendingTransactions;
+                };
+            });
+
+
+            if (!newLongestChain || (newLongestChain && !UCVcoin.chainIsValid(newLongestChain))) {
+                res.json({
+                    note: 'Current chain has not been replaced.',
+                    chain: UCVcoin.chain
+                });
+            }
+            else {
+                UCVcoin.chain = newLongestChain;
+                UCVcoin.pendingTransactions = newPendingTransactions;
+                res.json({
+                    note: 'This chain has been replaced.',
+                    chain: UCVcoin.chain
+                });
+            }
+        });
+});
+
 
 
 app.listen(port, () => {
